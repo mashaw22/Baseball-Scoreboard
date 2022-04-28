58ab9f3f72332b1cb082a4903c7361a495fd076f
Samantha Neely <samneely.dev@gmail.com>
2022-04-28 17:09:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <main className="main">
+            <div>
+                <h2 className="title">HOME</h2>
+                <h2 className="score">00</h2>
+                <p className="sub-title">BALL</p>
+                <div className="ball-section"></div>
+            </div>
+
+            <div>
+                <h2 className="inning">6</h2>
+                <h2 className="title">INNING</h2>
+                <p className="sub-title">STRIKE</p>
+                <div className="strike-section"></div>
+            </div>
+
+            <div>
+                <h2 className="title">GUEST</h2>
+                <h2 className="score">12</h2>
+                <p className="sub-title">OUT</p>
+                <div className="out-section"></div>
+            </div>
+        </main>
+    )
 }
-
-export default App;
