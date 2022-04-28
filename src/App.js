e1b375ff61bc78440179e0e488aa569f9a30f2fd
Samantha Neely <samneely.dev@gmail.com>
2022-04-28 17:18:24 -0500

    added ball strike and out tallies

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ export default function App() {
                 <h2 className="title">HOME</h2>
                 <h2 className="score">00</h2>
                 <p className="sub-title">BALL</p>
-                <div className="ball-section"></div>
+                <div className="ball-section">
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                </div>
             </div>
 
             <div>
                 <h2 className="inning">6</h2>
                 <h2 className="title">INNING</h2>
                 <p className="sub-title">STRIKE</p>
-                <div className="strike-section"></div>
+                <div className="strike-section">
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                </div>
             </div>
 
             <div>
                 <h2 className="title">GUEST</h2>
                 <h2 className="score">12</h2>
                 <p className="sub-title">OUT</p>
-                <div className="out-section"></div>
+                <div className="out-section">
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                </div>
             </div>
         </main>
     )
