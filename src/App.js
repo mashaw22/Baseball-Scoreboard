c54c82b399b13de57b598cc0dd1dc2326de66e04
Samantha Neely <samneely.dev@gmail.com>
2022-04-28 22:37:49 -0500

    added responsive warning for small screens

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from "react"
 export default function App() {
     return (
         <main className="main">
+            <section className="overlay">For best results, rotate device to landscape mode.</section>
             <div>
                 <h2 className="title">HOME</h2>
-                <h2 className="score">00</h2>
+                <h2 className="score-display">00</h2>
                 <p className="sub-title">BALL</p>
                 <div className="ball-section">
                     <div className="ball"></div>
@@ -15,7 +16,7 @@ export default function App() {
             </div>
 
             <div>
-                <h2 className="inning">6</h2>
+                <h2 className="inning-display">6</h2>
                 <h2 className="title">INNING</h2>
                 <p className="sub-title">STRIKE</p>
                 <div className="strike-section">
@@ -26,7 +27,7 @@ export default function App() {
 
             <div>
                 <h2 className="title">GUEST</h2>
-                <h2 className="score">12</h2>
+                <h2 className="score-display">12</h2>
                 <p className="sub-title">OUT</p>
                 <div className="out-section">
                     <div className="ball"></div>
