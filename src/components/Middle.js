3e59ecc4863a260dba02cb71a7b8b0fe59e65a60
Samantha Neely <samneely.dev@gmail.com>
2022-04-28 23:02:59 -0500

    Separated left, middle, and right sections

---
+++ src/components/Middle.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Middle() {
+    return(
+        <div>
+            <h2 className="inning-display">6</h2>
+            <h2 className="title">INNING</h2>
+            <p className="sub-title">STRIKE</p>
+            <div className="strike-section">
+                <div className="ball"></div>
+                <div className="ball"></div>
+            </div>
+        </div>
+    )
+}
