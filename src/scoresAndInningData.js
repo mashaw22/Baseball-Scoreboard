abc51aa9806dff5e88c47054e547c26f74ea8698
Samantha Neely <samneely.dev@gmail.com>
2022-05-01 04:30:36 -0500

    Added names to data sets

--- src/scoresAndInningData.js
+++ src/scoresAndInningData.js
@@ -1,4 +1,4 @@
-export default [
+const scoresAndInningData = [
     {
         name: "HOME",
         value: 0,
@@ -15,3 +15,5 @@ export default [
         minusIsDisplayed: false
     }
 ]
+
+export default scoresAndInningData
